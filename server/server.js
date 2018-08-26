f441c5669874e5229b8d8223d2437fc32061e3c5
zpzhou <zpzhou@sfu.ca>
2018-08-26 15:48:51 -0700

    setup socket.io

--- server/server.js
+++ server/server.js
@@ -1,15 +1,28 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 const bodyParser = require('body-parser');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server); // create IO server
+
 const port = process.env.PORT || 8080;
 const publicPath = path.join(__dirname, '../public');
+
 
 app.use(express.static(publicPath));
 app.use(bodyParser.json()); // JSON to post handler
 
+// register event listenr
+io.on('connection', (socket) => {
+    console.log('New user connected');
+    socket.on('disconnect', (socket) => {
+        console.log('Client disconnected!');
+    });
+});
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
